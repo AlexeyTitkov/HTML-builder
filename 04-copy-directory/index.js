9c5d421fd47bbde7b4de6e181eb02dc8d5853354
AlexeyTitikov <alexey.titkov.work@gmail.com>
2023-05-10 09:40:20 +0300

    fix solution of task (4)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -20,7 +20,28 @@ async function copyDir(sourceDir, targetDir) {
       await copyDir(sourceFile, targetFile);
     }
   }
+
+  const targetFiles = await fs.readdir(targetDir);
+  for (const targetFile of targetFiles) {
+    const sourceFilePath = path.join(sourceDir, targetFile);
+    const targetFilePath = path.join(targetDir, targetFile);
+    const sourceFileExists = await fileExists(sourceFilePath);
+
+    if (!sourceFileExists) {
+      await fs.unlink(targetFilePath); // Удаление файлов, которых нет в папке files
+    }
+  }
+
   console.log(`Папка files скопирована в ${targetDir}`);
+}
+
+async function fileExists(filePath) {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 const sourceDir = path.join(__dirname, 'files');
